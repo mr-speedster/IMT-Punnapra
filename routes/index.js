d33963905b4954025fdaa00a41593ca64a638589
mr-speedster <ajithd78564@gmail.com>
2023-05-13 00:24:59 +0530

    updated with route setup

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,24 @@ router.get('/about-us', function(req, res, next) {
   res.render('about-us');
 });
 
+router.get('/admission', function(req, res, next) {
+  res.render('admission');
+});
+
+router.get('/our-course', function(req, res, next) {
+  res.render('our-course');
+});
+
+router.get('/facilities', function(req, res, next) {
+  res.render('facilities');
+});
+
+router.get('/faculty', function(req, res, next) {
+  res.render('faculty');
+});
+
+router.get('/contact-us', function(req, res, next) {
+  res.render('contact-us',);
+});
+
 module.exports = router;
